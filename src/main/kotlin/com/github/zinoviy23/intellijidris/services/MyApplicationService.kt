79434bd7ed7983ddcb717603cef0089faf32ad96
GitHub Action <action@github.com>
2022-07-09 19:51:41 +0000

    Template cleanup

--- src/main/kotlin/com/github/zinoviy23/intellijidris/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/zinoviy23/intellijidris/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.zinoviy23.intellijidris.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.zinoviy23.intellijidris.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/zinoviy23/intellijidris/services/MyProjectService.kt
+++ src/main/kotlin/com/github/zinoviy23/intellijidris/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.zinoviy23.intellijidris.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.zinoviy23.intellijidris.MyBundle
 
 class MyProjectService(project: Project) {
 
